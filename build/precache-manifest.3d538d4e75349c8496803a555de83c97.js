1515ce9dce9aaf3a6cc9680de501b4087f1b0088
JuzTroll <ciprianwang@gmail.com>
2023-04-21 17:05:08 +0300

    working react app

---
+++ build/precache-manifest.3d538d4e75349c8496803a555de83c97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd31a9b243eb4042c858d8ac5a8352b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d54b302c6375acc4b3f",
+    "url": "/static/css/main.15e07dc9.chunk.css"
+  },
+  {
+    "revision": "9982e9c44945e707350c",
+    "url": "/static/js/2.d086f779.chunk.js"
+  },
+  {
+    "revision": "6d54b302c6375acc4b3f",
+    "url": "/static/js/main.28f332de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
