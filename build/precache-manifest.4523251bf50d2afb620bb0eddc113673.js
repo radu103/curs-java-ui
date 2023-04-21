1449b4945f5ebafefcf53ab20d3e012663c94b0c
Ficus12 <andrei.tdr767@gmail.com>
2023-04-21 17:05:53 +0300

    starter

---
+++ build/precache-manifest.4523251bf50d2afb620bb0eddc113673.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02552d94229fd791e6b9f8f65486694b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d54b302c6375acc4b3f",
+    "url": "/static/css/main.15e07dc9.chunk.css"
+  },
+  {
+    "revision": "9982e9c44945e707350c",
+    "url": "/static/js/2.d086f779.chunk.js"
+  },
+  {
+    "revision": "6d54b302c6375acc4b3f",
+    "url": "/static/js/main.28f332de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- js/script.js
+++
@@ -1,5 +0,0 @@
-function changeColor(){
-    document.body.className = 
-    (document.body.className === "bodyStyleLightBlue") ? 
-    "bodyStyleYellow" : "bodyStyleLightBlue";
-}
