2452236942eca4b92c47f3f9e242d0d01d25fc21
Radu Simen <radu.simen@chooose.today>
2023-04-28 16:51:52 +0300

    cars from backend displayed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 import { ThemeProvider } from "react-ui";
 import { tokens, components } from "react-ui/themes/light";
 
-import Cars from "./cars";
+// import Cars from "./cars";
+
+function CarRow(props) {
+  return <div>I am {props.brand} and my color is { props.color }</div>;
+}
 
 export default function App() {
+
+  const [cars, setCars ] = useState([]);
+
+  function fetchCarData(){
+    fetch("http://localhost:8080/v1/car/list")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setCars(data);
+      })
+  }
+
+  useEffect(() => {
+    fetchCarData()
+  }, []);
+
   return (
     <ThemeProvider tokens={tokens} components={components}>
-      <Cars/>
+        <h1>Who lives in my garage?</h1>
+        { cars.map((car) => <CarRow brand={car.maker} color={car.color} />)}
     </ThemeProvider>
   );
 }
