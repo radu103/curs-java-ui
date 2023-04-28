8d2f8145404d09664dedde8f7e7bfca77aafea86
robert <rnechita06@gmail.com>
2023-04-28 16:07:09 +0300

    da

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from "react";
 import "./styles.css";
 
-import { ThemeProvider, Link } from "react-ui";
+import { ThemeProvider } from "react-ui";
 import { tokens, components } from "react-ui/themes/light";
 
-import Examples from "./examples";
+import Cars from "./cars";
 
 export default function App() {
   return (
     <ThemeProvider tokens={tokens} components={components}>
-      <Link
-        href="https://react-ui.dev/components"
-        target="_blank"
-        marginBottom={4}
-      >
-        Open documentation
-      </Link>
-      {<Examples />}
+      
+      {<Cars />}
     </ThemeProvider>
   );
 }
---
+++ src/cars/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Stack } from "react-ui";
+
+const carsList = [
+    {
+        brand : 'Ford',
+        no : 1
+    },
+    {
+        brand : 'BMW',
+        no : 2
+    },
+    {   
+        brand : 'Audi',
+        no : 3
+    }
+];
+
+function CarRow(pr) {
+    return <div>{pr.no}. I am a { pr.brand }</div>;
+}
+
+function Cars() {
+    return (
+      <>
+        <h1>Who lives in my garage?</h1>
+        {carsList.map((car) => <CarRow brand={car.brand} no={car.no} />)}
+      </>
+    );
+}
+  export default Cars;
