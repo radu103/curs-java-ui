0cefe23d9f76a66edb11c52a21830ee3ef91f355
MobR10 <anghelraffaele10@gmail.com>
2023-05-05 16:52:26 +0300

    lectie

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-function changeColor() {
-    document.body.className=
-    (document.body.className ==="bodyStyleGrey") ?
-        "bodyStyleYellow" : "bodyStyleGrey";
-  
-}
+let carsTable=null;
+let cars=[];
+
+
+$(document).ready(function() {
+
+    $.getJSON('http://localhost:8080/v1/car/list',function(data){
+        cars=data;
+        carsTable=new DataTable('#carsTable');
+    });
+
+    $('#carsTable').DataTable({
+        ajax: 'http://localhost:8080/v1/car/list'
+    })
+
+    
+});
