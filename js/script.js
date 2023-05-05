9f22b733aaacb0aa46cf9b3dfe85bbaab7851101
cygnushg <45734699+cygnushg@users.noreply.github.com>
2023-05-05 16:17:15 +0300

    jquery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-    function changeColor() {
-        document.body.className = (document.body.className === "bodyStyleGrey") ? "bodyStyleYellow" : "bodyStyleGrey";
+function changeColor() {
+    let $body = $("#htmlBody");
+    let $spans = $body.children("span");
+
+    for(let i = 0; i < $spans.length; i++) {
+        let $span = $($spans.get(i));
+        $span.toggleClass("spanClass");
     }
+
+    if($body.hasClass("bodyStyleGrey")) {
+        $body.removeClass("bodyStyleGrey");
+        $body.addClass("bodyStyleYellow");
+    } else {
+        $body.addClass("bodyStyleGrey");
+        $body.removeClass("bodyStyleYellow");
+    }
+}
+    // document.body.className = (document.body.className === "bodyStyleGrey") ? "bodyStyleYellow" : "bodyStyleGrey";
+
+
+$(document).ready(function() {
+    alert("Hello World");
+});
+
+$('#changeColorBtn').click(changeColor);
