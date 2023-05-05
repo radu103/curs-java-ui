3993e700a600bdc5d43f8513c90f05fe1b674907
robert <rnechita06@gmail.com>
2023-05-05 15:17:29 +0300

    da

--- src/cars/index.js
+++ src/cars/index.js
@@ -1,31 +1,18 @@
 import React from "react";
-import { Stack } from "react-ui";
+// import { Stack } from "react-ui";
 
-const carsList = [
-    {
-        brand : 'Ford',
-        no : 1
-    },
-    {
-        brand : 'BMW',
-        no : 2
-    },
-    {   
-        brand : 'Audi',
-        no : 3
-    }
-];
-
-function CarRow(pr) {
-    return <div>{pr.no}. I am a { pr.brand }</div>;
+function CarRow(props) {
+    return <div>I am {props.maker} and my color is { props.color }</div>;
 }
 
 function Cars() {
+
     return (
       <>
         <h1>Who lives in my garage?</h1>
-        {carsList.map((car) => <CarRow brand={car.brand} no={car.no} />)}
+        { this.state.cars.map((car) => <CarRow brand={car.maker} color={car.color} />)}
       </>
     );
 }
-  export default Cars;
+
+export default Cars;
