23ffc0f9a73eb7dbb82805f95f97599980369592
MobR10 <anghelraffaele10@gmail.com>
2023-05-05 16:58:42 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 function changeColor() {
-    document.body.className=
-    (document.body.className ==="bodyStyleGrey") ?
-        "bodyStyleYellow" : "bodyStyleGrey";
-  
+    let $body = $("#htmlBody");
+    let $spans = $body.children("span");  // get all children of body that are span elements
+
+    for(let i = 0; i < $spans.length; i++) {
+        let $span = $($spans.get(i));
+        $span.toggleClass("spanClass");
+    }
+
+    if($body.hasClass("bodyStyleGrey")) {
+        $body.removeClass("bodyStyleGrey");
+        $body.addClass("bodyStyleYellow");
+    } else {
+        $body.removeClass("bodyStyleYellow");
+        $body.addClass("bodyStyleGrey");
+    }
 }
+
+$(document).ready(function() {
+    alert("Hello World!");
+});
+
+$('#changeColorBtn').click(changeColor);
