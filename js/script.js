bc85f96a363b18c70b900139180b148a683d4948
andrei <andrei.tdr767@gmail.com>
2023-05-05 16:47:23 +0300

    yes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-function changeColor()
-{
-    document.body.className = 
-        (document.body.className === "bodyStyleGreen") ?
-            "bodyStyleYellow" : "bodyStyleGreen";
-}
+let carsTable = null;
+let cars = [];
+
+$(document).ready(function(){
+    $.getJSON('http://localhost/v1/car/list', function(data) {
+        cars = data;
+        carsTable = new DataTable('#carsTable');
+        carsTable.data()
+    })
+});
+
