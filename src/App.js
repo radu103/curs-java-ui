2e2a688be842416c37eddfa77df07673b0f03c57
cygnushg <45734699+cygnushg@users.noreply.github.com>
2023-05-05 15:02:54 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./styles.css";
 
-import { ThemeProvider, Link } from "react-ui";
+import { ThemeProvider } from "react-ui";
 import { tokens, components } from "react-ui/themes/light";
 
-import Cars from "./cars";
+import Cars, { moreExpensive } from "./cars";
 
 export default function App() {
   return (
     <ThemeProvider tokens={tokens} components={components}>
-      <Cars/>
+        <Cars/>
     </ThemeProvider>
   );
 }
--- src/cars/index.js
+++ src/cars/index.js
@@ -1,31 +1,33 @@
-import React from "react";
-import { Stack } from "react-ui";
+import React, { useEffect, useState } from "react";
+// import { Stack } from "react-ui";
+// import App from 'App.js'
 
-const carsList = [
-    {
-        brand : 'Ford',
-        no : 1
-    }, 
-    {
-        brand : 'BMW',
-        no : 2
-    }, 
-    {   
-        brand : 'Audi',
-        no : 3
-    }
-];
-
-function CarRow(pr) {
-    return <div>{pr.no}. I am a { pr.brand }</div>
+function CarRow(props) {
+    return <div>I am {props.brand} and my color is { props.color } and the price is { props.price } { props.curr }</div>;
 }
 
-function Cars() {  
+function Cars() {
+  const [cars, setCars ] = useState([]);
+
+  function fetchCarData(params){
+    fetch("http://localhost:80/v1/car/list" + params)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setCars(data);
+      })
+  }
+
+  useEffect(() => {
+    fetchCarData("");
+  }, []);
     return (
-        <>
-            <h1>Who lives in my garage?</h1>
-            {carsList.map((car) => <CarRow brand={car.brand} no={car.no}/>)}
-        </>
+      <>
+        <h1>Who lives in my garage?</h1>
+        { cars.map((car) => <CarRow brand={car.maker} color={car.color} price={car.price.price} curr={car.price.currency} />)}
+      </>
     );
 }
+
 export default Cars;
