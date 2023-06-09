a5ca3ee589be974f585cbbfff38d3cd9b2455a2e
cygnushg <45734699+cygnushg@users.noreply.github.com>
2023-06-09 11:39:39 +0300

    chestie

--- js/script.js
+++ js/script.js
@@ -1,32 +1,106 @@
 let carsForTable = [];
+let carsDataTable = null;
 
-function convertToTableData(car) {
+function _convertToTableData(car) {
     let carForTable = [];
-    
+
+    carForTable.push(car.id);
     carForTable.push(car.maker);
     carForTable.push(car.model);
-    carForTable.push(car.color);
     carForTable.push(car.year);
-    carForTable.push(car.price.price + " " + car.price.currency);
+    carForTable.push(car.color);
+    carForTable.push(car.price + " " + car.currency);
 
     return carForTable;
 }
 
-$(document).ready(function() {
-    $.getJSON("http://localhost/v1/car/list", function(data) {
+function _createTable(carsForTable) {
+    carsDataTable = $('#carsTable').DataTable({
+        data: carsForTable,
+        columns: [
+            { title: "ID", className: 'dt-center', targets: 1 },
+            { title: "Maker", className: 'dt-center', targets: 1 },
+            { title: "Model", className: 'dt-center', targets: 1 },
+            { title: "Year", className: 'dt-center', targets: 1 },
+            { title: "Color", className: 'dt-center', targets: 1 },
+            { title: "Price", className: 'dt-right', targets: 1 }
+        ]
+    });
+}
+
+function _loadTableData() {
+    $.getJSON("http://localhost:8080/v2/car/list", function (data) {
+
+        carsForTable = [];
         for (const car of data) {
-            carsForTable.push(convertToTableData(car));
+            carsForTable.push(_convertToTableData(car));
         }
 
-        $("#carsTable").DataTable({
-            data : carsForTable,
-            columns : [
-                { title: "Maker" },
-                { title: "Model" },
-                { title: "Year" },
-                { title: "Color" },
-                { title: "Price" }
-            ]
-        });
-    })
+        if (carsDataTable === null) {
+            _createTable(carsForTable);
+        }
+        else {
+            carsDataTable.destroy();
+            _createTable(carsForTable);
+        }
+    });
+}
+
+$(document).ready(function () {
+    _loadTableData();
+});
+
+$('#createCarBtn').click(function () {
+    let car = {
+        maker: document.getElementById("makerInput").value,
+        model: document.getElementById("modelInput").value,
+        year: document.getElementById("yearInput").value,
+        color: document.getElementById("colorInput").value,
+        price: document.getElementById("priceInput").value,
+        currency: document.getElementById("currencyInput").value
+    };
+
+    $.ajax({
+        url: "http://localhost:8080/v2/car/add",
+        type: "POST",
+        data: JSON.stringify(car),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            alert("Car created!");
+            _loadTableData();
+        },
+        error: function (xhr, status, error) {
+            alert(xhr.responseText);
+        }
+    });
+
+});
+
+$('#updateCarBtn').click(function () {
+    let car = {
+        id: document.getElementById("idInput1").value,
+        maker: document.getElementById("makerInput1").value,
+        model: document.getElementById("modelInput1").value,
+        year: document.getElementById("yearInput1").value,
+        color: document.getElementById("colorInput1").value,
+        price : {
+            price : document.getElementById("priceInput1").value,
+            currency: document.getElementById("currencyInput1").value
+        }
+    };
+
+    $.ajax({
+        url: "http://localhost:8080/v2/car/update/" + car.id,
+        type: "POST",
+        data: JSON.stringify(car),
+        contentType: "application/json",
+        success: function (data) {
+            alert("Car updated!");
+            _loadTableData();
+        },
+        error: function (xhr, status, error) {
+            alert(xhr.responseText);
+        }
+    });
 });
